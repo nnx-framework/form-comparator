11fea6e4021e55a3bad076040e285b598f6d25d4
Malofeykin Andrey <malofeykin@platformasoft.ru>
2016-09-20 16:41:36 +0300

    Сравнение элементов формы

---
+++ src/Comparator/Diff/InsertedElementInterface.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @link    https://github.com/nnx-framework/form-comparator
+ * @author  Malofeykin Andrey  <and-rey2@yandex.ru>
+ */
+namespace Nnx\FormComparator\Comparator\Diff;
+
+/**
+ * Interface InsertedElementInterface
+ *
+ * @package Nnx\FormComparator\Comparator\Diff
+ */
+interface InsertedElementInterface
+{
+
+}
---
+++ src/Comparator/Diff/UpdatedElementInterface.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @link    https://github.com/nnx-framework/form-comparator
+ * @author  Malofeykin Andrey  <and-rey2@yandex.ru>
+ */
+namespace Nnx\FormComparator\Comparator\Diff;
+
+/**
+ * Interface UpdatedElementInterface
+ *
+ * @package Nnx\FormComparator\Comparator\Diff
+ */
+interface UpdatedElementInterface
+{
+
+}
